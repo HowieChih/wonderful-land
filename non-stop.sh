93881f17630d6b8f19a17929ae4a2f0271645701
qihao <howard.qihao@kalerm.com>
2025-08-05 09:31:36 +0800

    update

---
+++ non-stop.sh
@@ -0,0 +1,2 @@
+export https_proxy=http://127.0.0.1:7890;export http_proxy=http://127.0.0.1:7890;export all_proxy=socks5://127.0.0.1:7890
+git add . && git commit -m 'update' && git push origin main
